210ba147af7a15cf3ff820d049e135f0d61d1541
Perimeter-hub <tenzoretto@gmail.com>
2026-07-17 13:23:55 +0200

    Add Site Audit route

---
+++ app/site-audit/page.js
@@ -0,0 +1,9 @@
+import SiteAuditWorkspace from "../../components/site-audit/SiteAuditWorkspace";
+
+export const metadata = {
+  title: "Site Audit | Sentinel Autogard"
+};
+
+export default function SiteAuditPage() {
+  return <SiteAuditWorkspace />;
+}
